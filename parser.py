7b04ddd19cb05a4d97063d7dfc9f848ec11327cf
ZarvisDaemon <13596702+ZarvisD@users.noreply.github.com>
2019-03-25 18:15:48 +0530

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+import re
+import unicodedata
+raw_html =  open('pass.html').read() # Opening file for reading
+soup = BeautifulSoup(raw_html, 'lxml') # Parse the HTML as a string
+table = soup.find_all('table') # Grab the first table
+
+for i in range(len(table)):
+    con = table[i].find_all('tr')
+    # print con[0]
+    vendor_name = con[0].find_all('td')[0].find_all('b')[0].contents
+    # print vendor_name[0].string
+    # print vendor_name[0].decode('utf-8') + vendor_name [1].decode('utf-8')
+    p=unicodedata.normalize("NFKD", vendor_name[0].string)
+    p1=unicodedata.normalize("NFKD", vendor_name[1].string)
+    print '\n' + p.decode('utf-8') + p1.decode('utf-8')
+    for j in range(1,len(con)):
+        td = con[j].find_all('td')
+        # print td
+        # print type(td[0].find_all('b')[0])
+        name = td[0].find_all('b')[0].contents
+        if td[1].find('a'):
+            value = td[1].find('a').contents
+        else:
+            value = td[1].contents
+        print name[0].decode('utf-8') + ' : ' + value[0].decode('utf-8')
+    
+
+
+
+
+# print con[j].find_all('td')[0].find_all('b')[0].contents
